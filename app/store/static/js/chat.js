5d5779906f6b99b168c21de1a3f04823d073ab40
AndrewDya <andrewdya@gmail.com>
2023-11-07 05:48:50 +0600

    new functions

---
+++ app/store/static/js/chat.js
@@ -0,0 +1,9 @@
+// chat.js
+document.addEventListener("DOMContentLoaded", function () {
+    const chatIcon = document.getElementById("chatIcon");
+
+    chatIcon.addEventListener("click", function () {
+        // Здесь добавьте код для открытия чата или поддержки
+        alert("Открываем чат!");
+    });
+});
