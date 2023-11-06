42f37050d867839ffc51ef56314771a0d8e769c5
AndrewDya <andrewdya@gmail.com>
2023-11-07 03:48:58 +0600

    db added, game_1 added, templates refacor

--- app/store/urls.py
+++ app/store/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from store import views
 
 urlpatterns = [
-    path('', views.home, name='home.html'),
+    path('', views.home, name='home'),
+    path('game/<int:game_id>/', views.game_detail, name='game_detail')  # Изменено имя URL-шаблона
     # Другие маршруты для других страниц магазина
 ]
--- app/store/views.py
+++ app/store/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from store.models import Game
+
 
 def home(request):
     return render(request, 'store/home.html')
 
+
+def game_detail(request, game_id):
+    # Получите информацию о конкретной игре из базы данных (по её ID)
+    game = Game.objects.get(id=game_id)
+    # Здесь вы можете передать информацию о игре в шаблон и отобразить её
+    return render(request, 'games/game_detail.html', {'game': game})
